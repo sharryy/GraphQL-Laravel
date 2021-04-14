e06cc5956b942e4be29ba8f3c82b258997ced36b
Shehryar Amin <ibneadam388@gmail.com>
2021-04-14 15:49:27 +0500

    Query Type Added

---
+++ app/GraphQL/Queries/BookQuery.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Book;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+
+
+class BookQuery
+{
+    protected $attributes = [
+        'name' => 'book',
+    ];
+
+    public function type(): Type
+    {
+        return GraphQL::type('Book');
+    }
+
+    public function args(): array
+    {
+        return [
+            'id' => [
+                'name' => 'id',
+                'type' => Type::int(),
+                'rules' => ['required']
+            ],
+        ];
+    }
+
+    public function resolve($root, $args)
+    {
+        return Book::findOrFail($args['id']);
+    }
+}
